f8e0c99359b1bb725a9b52fa0681965264d91f98
masoud <mwsoush.84@gmail.com>
2023-10-31 19:40:56 -0700

    api server configuration has been added

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
+import (
+	"github.com/go-chi/chi"
+	"github.com/joho/godotenv"
+	"log"
+	"net/http"
+	"os"
+)
+
 func main() {
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Fatal("somthing went wrong :", err)
+	}
+	port := os.Getenv("PORT")
+	host := os.Getenv("HOST")
+	if port == "" || host == "" {
+		log.Fatal("there is no host or port for running the project")
+		os.Exit(1)
+	}
+	router := chi.NewRouter()
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+	srv.ListenAndServe()
 
 }
